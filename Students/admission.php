ad6da73a22172383d49bfa4c8af8877611845d97
stanleybishesh <unikbisheshshrestha83@gmail.com>
2024-02-29 23:04:51 +0545

    admission form validation and other related changes

--- Students/admission.php
+++ Students/admission.php
@@ -1,4 +1,6 @@
 <?php
+session_start();
+error_reporting(0);
 $servername = "localhost";
 $username = "root";
 $password = "";
@@ -44,9 +46,14 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $studentPhotoPath = $studentUpload . $studentPhoto;
     move_uploaded_file($_FILES["studentPhoto"]["tmp_name"], $studentPhotoPath);
 
-    $citizenshipPhoto = $_FILES["citizenshipPhoto"]["name"];
-    $citizenshipPhotoPath = $citizenshipUpload . $citizenshipPhoto;
-    move_uploaded_file($_FILES["citizenshipPhoto"]["tmp_name"], $citizenshipPhotoPath);
+    // Handle multiple file uploads for citizenship photo
+    $citizenshipPhotos = $_FILES["citizenshipPhoto"];
+    foreach ($citizenshipPhotos["tmp_name"] as $key => $tmp_name) {
+        $citizenshipPhoto = basename($citizenshipPhotos["name"][$key]);
+        $citizenshipPhotoPath = $citizenshipUpload . $citizenshipPhoto;
+        move_uploaded_file($tmp_name, $citizenshipPhotoPath);
+        // Now $citizenshipPhotoPath contains the path of each uploaded file
+    }
 
     $marksheet10 = $_FILES["marksheet10"]["name"];
     $marksheet10Path = $marksheet10Upload . $marksheet10;
@@ -57,13 +64,15 @@ if ($_SERVER["REQUEST_METHOD"] == "POST") {
     move_uploaded_file($_FILES["marksheet12"]["tmp_name"], $marksheet12Path);
 
     // SQL query to insert data into the database
-    $sql = "INSERT INTO admission_users (first_name, last_name, course, student_photo, father_name, mother_name, dob, nationality, gender, citizenship, citizenship_photo, temp_address, per_address, board10, year10, percentage10, stream10, board12, year12, percentage12, stream12, marksheet10, marksheet12, declaration) VALUES ('$firstName','$lastName', '$course', '$studentPhoto', '$fatherName', '$motherName', '$dob', '$nationality', '$gender', '$citizenship', '$citizenshipPhoto', '$tempAddress', '$perAddress', '$board10', '$year10', '$percentage10', '$stream10', '$board12', '$year12', '$percentage12', '$stream12', '$marksheet10', '$marksheet12', '$declaration')";
+    $sql = "INSERT INTO admission_users (user_results_id, first_name, last_name, course, student_photo, father_name, mother_name, dob, nationality, gender, citizenship, citizenship_photo, temp_address, per_address, board10, year10, percentage10, stream10, board12, year12, percentage12, stream12, marksheet10, marksheet12, declaration) VALUES ('{$_SESSION['result_id']}', '$firstName','$lastName', '$course', '$studentPhoto', '$fatherName', '$motherName', '$dob', '$nationality', '$gender', '$citizenship', '$citizenshipPhoto', '$tempAddress', '$perAddress', '$board10', '$year10', '$percentage10', '$stream10', '$board12', '$year12', '$percentage12', '$stream12', '$marksheet10', '$marksheet12', '$declaration')";
 
     // Perform the query
     if ($conn->query($sql) === TRUE) {
         echo "<script>alert('Record Inserted Successfully !!');</script>";
+        echo "<script>window.location.href ='dashboard.php'</script>";
     } else {
-        echo "Error: " . $sql . "<br>" . $conn->error;
+            // echo "Error: " . $sql . "<br>" . $conn->error;
+        echo "<script>alert('Form submission failed !!');</script>";
         echo "<script>window.location.href ='form.php'</script>";
     }
 }
